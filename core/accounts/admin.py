9543728bb584df7cde7ff730a9e6b277b9bfa891
FahimReza <FahimReza202@outlook.com>
2024-11-20 00:59:07 +0330

    Create The Admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+
+class AdminCustom(UserAdmin):
+    model = User
+    list_display = ("email","is_superuser","is_active","is_staff")
+    list_filter = ("email" , "is_superuser" , "is_active",)
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (
+            "Authenticated",{
+                "fields":
+                (
+                    "email",
+                    "password",
+                ),
+            },
+        ),
+        (
+            "Permissions",{
+                "fields":(
+                    "is_staff",
+                    "is_superuser",
+                    "is_active"
+                ),
+            },
+        ),
+        (
+            "Group Permissions",
+            {
+                "fields": ("groups", "user_permissions"),
+            },
+        ),
+        (
+            "Importent Dates",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, AdminCustom)
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager , PermissionsMixin , AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
@@ -21,9 +25,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser' , True)
         extra_fields.setdefault('is_active' , True)
 
-        if extra_fields.get('is_staff') != True:
+        if extra_fields.get('is_staff') is not True:
             raise ValueError(_("Super User Must Have is_staff==True."))
-        if extra_fields.get("is_superuser") != True:
+        if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(email , password , **extra_fields)
     
@@ -34,12 +38,12 @@ class User(AbstractBaseUser , PermissionsMixin):
     email = models.EmailField(unique=True , max_length=255)
     is_staff=models.BooleanField(default=False)
     is_superuser=models.BooleanField(default=False)
-    is_active=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
 
     first_name = models.CharField(max_length=20)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELD = ["email",]
+    REQUIRED_FIELDS = []
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
