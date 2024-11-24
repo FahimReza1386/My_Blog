6084ff66d34642c6f957fcf8f694780845690529
FahimReza <FahimReza202@outlook.com>
2024-11-24 19:56:15 +0330

    Delete Test Class

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,23 +5,3 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly # type: ignore
 from rest_framework.filters import SearchFilter , OrderingFilter # type: ignore
 from rest_framework.decorators import action # type: ignore
 from rest_framework.response import Response # type: ignore
-
-
-class BlogModelView(viewsets.ModelViewSet):
-    serializer_class = TaskSerializer
-    filterset_fields = ['Ticked']
-    search_fields = ['Text']
-    ordering_fields = ['Ticked']
-    # pagination_class = PermissionPagination
-
-    def get_queryset(self): 
-        query= Todo.objects.filter(user = self.request.user)
-        return query
-
-    @action(methods=['get'],detail=False)
-    def get_user_id(self , request):
-        id = request.user.id
-        return Response({'Your Id :':str(id)})
-    
-    def perform_create(self, serializer):  
-        serializer.save(user=self.request.user)  
