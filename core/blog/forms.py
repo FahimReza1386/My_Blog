adc6b23fe93b0beb2c604161a68acda1b1ba7345
FahimReza <FahimReza202@outlook.com>
2024-11-25 23:04:16 +0330

    Save The CHanged of blog/forms

--- core/blog/forms.py
+++ core/blog/forms.py
@@ -3,9 +3,9 @@ from .models import Post, Category
 
 
 class CreatePostForm(forms.ModelForm):
-    image = forms.CharField(label="تصویر خود را انتخاب نمایید ..." , help_text='', widget=forms.FileInput(attrs={'placeholder':"تصویر وبلاگ خود را بنویسید .."}))
+    image = forms.CharField(label="تصویر خود را انتخاب نمایید ..." ,required=False, help_text='', widget=forms.FileInput(attrs={'placeholder':"تصویر وبلاگ خود را بنویسید .."}))
     title = forms.CharField(label="" , help_text='', widget=forms.TextInput(attrs={'placeholder':"نام وبلاگ خود را بنویسید .."}))
-    content = forms.CharField(label="" , help_text='', widget=forms.TextInput(attrs={'placeholder':"متن وبلاگ خود را بنویسید .."}))
+    content = forms.CharField(label="" , max_length=200, help_text='', widget=forms.TextInput(attrs={'placeholder':"متن وبلاگ خود را بنویسید .."}))
     category = forms.ModelChoiceField(queryset=Category.objects.all(), label="" , help_text='', widget=forms.Select(attrs={'placeholder':"دسته بندی وبلاگ خود را انتخاب کنید .."}))
     published_date = forms.DateTimeField(label="" , help_text='', widget=forms.TextInput(attrs={'placeholder':"زمان انتشار وبلاگ خود را بنویسید .."}))
 
